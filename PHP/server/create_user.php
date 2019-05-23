26fe8daf67c68104b038b7cc365f9df710d50124
pcasalet <pcasalet@gmail.com>
2019-05-23 19:51:27 -0300

    Evaluacion Final

---
+++ PHP/server/create_user.php
@@ -0,0 +1,24 @@
+<?php
+
+    include 'conexionBD.php';
+    CrearUsuarios("Usuario1","1993/07/16","Usuario1@ipsum-systems.com","123456");
+    CrearUsuarios("Usuario2","1992/01/16","Usuario2@ipsum-systems.com","123456");
+    CrearUsuarios("Usuario3","1990/01/16","Usuario3@ipsum-systems.com","123456");
+
+    function CrearUsuarios($Nombre,$FechaNacimiento,$UserName,$Password){
+    IniciarConexion();
+    $Consulta="Select * from usuario where Username='".$UserName."'";
+    $Resultado= mysqli_num_rows($GLOBALS['Conexion']->query($Consulta));
+    if($Resultado==0){
+        $Consulta = "INSERT INTO usuario (Nombre, FechaNacimiento, Username, Password)
+        VALUES ('".$Nombre."', '".$FechaNacimiento."', '".$UserName."', '".password_hash($Password, PASSWORD_BCRYPT)."')";
+
+        if ($GLOBALS['Conexion']->query($Consulta) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $GLOBALS['Conexion']->error;
+        }
+    }
+    DesactivarConexion();
+    }
+ ?>
